6b10acdf9d065c7a4eee43d9e5d2078cfe7493d2
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-06-28 20:35:12 -0300

    feat: add new fields to sidebar
    - Specially the profile avatar and the button proffile
    - Imported phosphor icon
    - Included pencil icon on edit profile button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react';
 import style from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -5,11 +6,13 @@ export function Sidebar() {
         <aside className={style.sidebar}>
             <img className={style.cover} src="https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
             <div className={style.profile}>
+                <img className={style.avatar} src="https://avatars.githubusercontent.com/u/37700027?v=4" alt="Foto de perfil" />
                 <strong>Júlio Cezar Koronati</strong>
                 <span>Web Developer</span>
             </div>
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar perfil
                 </a>
             </footer>
