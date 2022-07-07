a405ca0cede5a3c824a73d9c567e3585132eefa9
Júlio Cezar Koronati <jckoronati@hotmail.com>
2022-07-06 21:20:44 -0300

    feat: implemented comment form structure

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,6 +21,13 @@ export function Post() {
                 <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p><a href="">#novoprojeto</a>{' '}<a href="">#nlw</a>{' '}<a href="">#rocketseat</a></p>
             </div>
+            <form className={style.commentForm}>
+                <strong>Deixe seu feedback</strong>
+                <textarea placeholder='Deixe seu comentário' />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     );
 }
